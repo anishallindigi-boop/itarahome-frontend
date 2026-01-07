7a213694a2a1de8521a4af4b80bbf7b85184ccc2
unknown <anishprajapati765@gmail.com>
2026-01-07 13:20:21 +0530

    Merge remote changes

--- app/elements/Footer.tsx
+++ app/elements/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-white py-16 px-6">
+    <footer className="bg-black text-white py-16 px-6">
       <div className="max-w-7xl mx-auto">
         <div className="grid md:grid-cols-4 gap-12 mb-12">
           <div>
